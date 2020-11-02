9ded79d65419394aed723fe8b622f4acb8a6432c
Miguel <mouramiguel016@gmail.com>
2020-11-02 19:58:23 +0000

    define directions and stop the snake to dissapear

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ snake[0] = {
 }
 let direction = "right"
 
+//creat background box
 function createBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+//creat snake
 function createSnake() {
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
@@ -20,7 +22,22 @@ function createSnake() {
     }
 }
 
+document.addEventListener("keydown", update);
+
+//key functions (up, down, left, right)
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function startGame() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+
     createBG();
     createSnake();
 
